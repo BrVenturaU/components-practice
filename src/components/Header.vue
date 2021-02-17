c27b7efff042bb942770734d14a8911f73cfabd9
BrVenturaU <brandonventura16@gmail.com>
2021-02-17 00:04:16 -0600

    Agrega encabezado de la página del proyecto (no se confirmo por error)

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-row>
+        <b-col cols="12">
+            <img alt="App Logo" class="logo rounded-circle shadow mb-3" src="@/assets/images/logo.jpg">
+            <h1>{{titulo}}</h1>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+export default {
+    name: 'Encabezado',
+    props: {
+        titulo: String
+  }
+}
+</script>
+
+<style scoped>
+    .logo{
+        width: 192px;
+        height: 192px;
+    }
+</style>
