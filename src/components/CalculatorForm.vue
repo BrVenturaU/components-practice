87e86b3e0348354c5093a850bdb935b790aa611d
BrVenturaU <brandonventura16@gmail.com>
2021-02-17 12:02:34 -0600

    Agrega metodo que escucha el evento de cambio.
    -Agrega propiedad para opcion seleccionada por defecto en las unidades
    de medida.
    -Crea función para obtener el resultado del calculo para optimizar el
    codigo.

--- src/components/CalculatorForm.vue
+++ src/components/CalculatorForm.vue
@@ -2,14 +2,15 @@
     <b-row class="justify-content-center mb-4">
         <b-col sm="5">
             <b-form-input
-                class="p-4 text-center text-dark input-text-size"
-                v-model="valueFrom"
-                :value="valueFrom"
+                class="text-center text-dark input-size"
+                v-bind:value="valueFrom"
+                @keyup="onKeyupValueFrom"
                 type="text"
             ></b-form-input>
             <select name="valueFrom" id="valueFrom" 
                 class="form-control form-select" 
                 @change="onChangeMeasureFrom">
+                <option :value="selectedMeasureUnit" selected>Selecciona</option>
                 <option v-for="measureUnit of measureUnits" :key="measureUnit.id" 
                     :value="measureUnit.id">{{measureUnit.name}}</option>
             </select>
@@ -19,13 +20,15 @@
         </b-col>
         <b-col sm="5">
             <b-form-input
-                class="p-4 text-center text-dark input-text-size"
+                class="text-center text-dark input-size"
                 v-model="valueResult"
                 type="text"
+                readonly
             ></b-form-input>
             <select name="valueResult" id="valueResult" 
                 class="form-control form-select"
                 @change="onChangeMeasureResult">
+                <option :value="selectedMeasureUnit" selected>Selecciona</option>
                 <option v-for="measureUnit of measureUnits" :key="measureUnit.id" 
                     :value="measureUnit.id">{{measureUnit.name}}</option>
             </select>
@@ -43,45 +46,43 @@ export default {
         return {
             valueFrom:1,
             valueResult: 1,
+            selectedMeasureUnit: 0,
             selectedMeasureUnitFrom: 0,
             selectedMeasureUnitResult: 0
         }
     },
     methods:{
         onChangeMeasureFrom(e){
-            // Obtenemos el valor de la unidad de medida (opcion) seleccionada
-            let measureUnitId = e.target.value;
-            console.log(measureUnitId);
-
-            /*
-                Logica de calculos acá si cambia la unidad de origen.
-                Se puede verificar si ese id es el de una unidad de medida especifica, al ser valores estaticos
-                ya se sabe cual sera, entonces si el id es igual a X numero se hace un calculo, sino otro.
-
-                O incluso seria mucho mas eficiente tener un switch case.
-
-            */
-
-            // Asignamos el resultado
-            this.valueResult = 12;
+            this.selectedMeasureUnitFrom = parseInt(e.target.value);
+            this.valueResult = getResult(this.valueFrom, this.selectedMeasureUnitFrom, this.selectedMeasureUnitResult);
         },
         onChangeMeasureResult(e){
-            // Obtenemos el valor de la unidad de medida (opcion) seleccionada
-            let measureUnitId = e.target.value;
-            console.log(measureUnitId);
-
-            /*
-                Logica de calculos acá si cambia la unidad de destino.
-                Se puede verificar si ese id es el de una unidad de medida especifica, al ser valores estaticos
-                ya se sabe cual sera, entonces si el id es igual a X numero se hace un calculo, sino otro.
-
-                O incluso seria mucho mas eficiente tener un switch case.
-
-            */
-
-            // Asignamos el resultado
-            this.valueResult = 13;
+            this.selectedMeasureUnitResult = parseInt(e.target.value);
+            this.valueResult = getResult(this.valueFrom, this.selectedMeasureUnitFrom, this.selectedMeasureUnitResult);
+        },
+        onKeyupValueFrom(e){
+            this.valueFrom = isNaN(parseInt(e.target.value)) ? "" : parseInt(e.target.value);
+            this.valueResult = getResult(this.valueFrom, this.selectedMeasureUnitFrom, this.selectedMeasureUnitResult);
         }
+    }
+}
+
+function getResult(valueFrom, selectedMeasureUnitFrom, selectedMeasureUnitResult){
+    // Obtenemos el valor de la unidad de medida.
+    let value = isNaN(parseInt(valueFrom)) ? "" : parseInt(valueFrom);
+    if(selectedMeasureUnitFrom != 0 && selectedMeasureUnitResult != 0 && value != ""){
+        /*
+            Logica de calculos acá si cambia la unidad de destino.
+            Se puede verificar si ese id es el de una unidad de medida especifica, al ser valores estaticos
+            ya se sabe cual sera, entonces si el id es igual a X numero se hace un calculo, sino otro.
+
+            O incluso seria mucho mas eficiente tener un switch case.
+
+        */
+
+        // Asignamos el resultado
+        let valueResult = 10 + value;
+        return valueResult;
     }
 }
 </script>
@@ -91,7 +92,9 @@ export default {
         margin-top: -2px;
     }
 
-    .input-text-size{
+    .input-size{
         font-size: 180%;
+        height: 46px;
+        padding: 1px 6px;
     }
 </style>
